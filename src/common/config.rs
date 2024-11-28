f1fad265446f8288602c0294ff6cb744d83e12aa
Staking Agency <contact@staking.agency>
2024-11-28 11:06:23 +0200

    added governance token

--- src/common/config.rs
+++ src/common/config.rs
@@ -1,6 +1,8 @@
 multiversx_sc::imports!();
 multiversx_sc::derive_imports!();
 
+use crate::common::errors::*;
+
 #[type_abi]
 #[derive(ManagedVecItem, TopEncode, TopDecode, NestedEncode, NestedDecode, PartialEq, Eq, Copy, Clone, Debug)]
 pub enum State {
@@ -10,9 +12,12 @@ pub enum State {
 
 #[multiversx_sc::module]
 pub trait ConfigModule {
+    // state
     #[only_owner]
     #[endpoint(setStateActive)]
     fn set_state_active(&self) {
+        require!(!self.governance_token().is_empty(), ERROR_TOKEN_NOT_SET);
+
         self.state().set(State::Active);
     }
 
@@ -25,4 +30,17 @@ pub trait ConfigModule {
     #[view(getState)]
     #[storage_mapper("state")]
     fn state(&self) -> SingleValueMapper<State>;
+
+    // governance token
+    #[only_owner]
+    #[endpoint(setGovernanceToken)]
+    fn set_governance_token(&self, token: TokenIdentifier) {
+        require!(self.governance_token().is_empty(), ERROR_TOKEN_ALREADY_SET);
+
+        self.governance_token().set(token);
+    }
+
+    #[view(getGovernanceToken)]
+    #[storage_mapper("governance_token")]
+    fn governance_token(&self) -> SingleValueMapper<TokenIdentifier>;
 }
---
+++ src/common/errors.rs
@@ -0,0 +1,2 @@
+pub static ERROR_TOKEN_NOT_SET: &[u8] = b"governance token not set";
+pub static ERROR_TOKEN_ALREADY_SET: &[u8] = b"governance token is already set";
