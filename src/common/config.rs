f070e076872afded596b6a720efdea55499a4bad
Staking Agency <contact@staking.agency>
2024-12-02 10:21:47 +0200

    finished implementation without tests

--- src/common/config.rs
+++ src/common/config.rs
@@ -10,6 +10,55 @@ pub enum State {
     Active,
 }
 
+#[type_abi]
+#[derive(TopEncode, TopDecode, NestedEncode, NestedDecode, ManagedVecItem)]
+pub struct Action<M: ManagedTypeApi> {
+    pub gas_limit: u64,
+    pub dest_address: ManagedAddress<M>,
+    pub payments: ManagedVec<M, ManagedBuffer<M>>,
+    pub endpoint_name: ManagedBuffer<M>,
+    pub arguments: ManagedVec<M, ManagedBuffer<M>>,
+}
+
+#[type_abi]
+#[derive(TopEncode, TopDecode)]
+pub struct ProposalCreationArgs<M: ManagedTypeApi> {
+    pub description: ManagedBuffer<M>,
+    pub actions: ManagedVec<M, Action<M>>,
+}
+
+#[type_abi]
+#[derive(TopEncode, TopDecode, NestedEncode, NestedDecode, PartialEq, Debug, Clone)]
+pub enum VoteType {
+    Upvote = 1,
+    DownVote = 2,
+}
+
+#[type_abi]
+#[derive(TopEncode, TopDecode, PartialEq, Debug)]
+pub enum ProposalStatus {
+    Pending, //Starts from 0
+    Active,
+    Defeated,
+    Succeeded,
+    Executed,
+}
+
+#[type_abi]
+#[derive(TopEncode, TopDecode)]
+pub struct Proposal<M: ManagedTypeApi> {
+    pub id: u64,
+    pub creation_block: u64,
+    pub proposer: ManagedAddress<M>,
+    pub description: ManagedBuffer<M>,
+
+    pub was_executed: bool,
+    pub actions: ManagedVec<M, Action<M>>,
+
+    pub num_upvotes: BigUint<M>,
+    pub num_downvotes: BigUint<M>,
+}
+
 #[multiversx_sc::module]
 pub trait ConfigModule {
     // state
@@ -17,6 +66,9 @@ pub trait ConfigModule {
     #[endpoint(setStateActive)]
     fn set_state_active(&self) {
         require!(!self.governance_token().is_empty(), ERROR_TOKEN_NOT_SET);
+        require!(!self.launchpad_sc().is_empty(), ERROR_LAUNCHPAD_SC_NOT_SET);
+        require!(self.quorum().get() > 0, ERROR_QUORUM_NOT_SET);
+        require!(self.voting_period().get() > 0, ERROR_VOTING_PERIOD_NOT_SET);
 
         self.state().set(State::Active);
     }
@@ -43,4 +95,94 @@ pub trait ConfigModule {
     #[view(getGovernanceToken)]
     #[storage_mapper("governance_token")]
     fn governance_token(&self) -> SingleValueMapper<TokenIdentifier>;
+
+    // launchpad sc
+    #[only_owner]
+    #[endpoint(setLaunchpadSC)]
+    fn set_launchpad_sc(&self, address: ManagedAddress) {
+        self.launchpad_sc().set(address);
+    }
+
+    #[view(getLaunchpadSC)]
+    #[storage_mapper("launchpad_sc")]
+    fn launchpad_sc(&self) -> SingleValueMapper<ManagedAddress>;
+
+    // min proposal amount
+    #[view(getMinProposalAmount)]
+    #[storage_mapper("min_proposal_amount")]
+    fn min_proposal_amount(&self) -> SingleValueMapper<BigUint>;
+
+    // last proposal id
+    #[view(getLastProposalId)]
+    #[storage_mapper("last_proposal_id")]
+    fn last_proposal_id(&self) -> SingleValueMapper<u64>;
+
+    // voting period (blocks)
+    #[view(getVotingPeriod)]
+    #[storage_mapper("voting_period")]
+    fn voting_period(&self) -> SingleValueMapper<u64>;
+
+    // quorum
+    #[view(getQuorum)]
+    #[storage_mapper("quorum")]
+    fn quorum(&self) -> SingleValueMapper<BigUint>;
+
+    // proposal
+    #[view(getProposal)]
+    #[storage_mapper("proposals")]
+    fn proposals(&self, id: u64) -> SingleValueMapper<Proposal<Self::Api>>;
+
+    // voters amounts
+    #[view(getVoterAmount)]
+    #[storage_mapper("voters_amounts")]
+    fn voters_amounts(&self, voter: &ManagedAddress, proposal_id: u64) -> SingleValueMapper<BigUint>;
+
+    // proposal voters
+    #[view(getProposalVoters)]
+    #[storage_mapper("proposal_voters")]
+    fn proposal_voters(&self, id: u64) -> UnorderedSetMapper<ManagedAddress>;
+
+    // voter proposals
+    #[view(getVoterProposals)]
+    #[storage_mapper("voter_proposals")]
+    fn voter_proposals(&self, voter: &ManagedAddress) -> UnorderedSetMapper<u64>;
+
+    // proposal status
+    #[view(getProposalStatus)]
+    fn get_proposal_status_view(&self, proposal_id: u64) -> ProposalStatus {
+        require!(!self.proposals(proposal_id).is_empty(), ERROR_PROPOSAL_NOT_FOUND);
+
+        let proposal = self.proposals(proposal_id).get();
+        self.get_proposal_status(&proposal)
+    }
+
+    fn get_proposal_status(&self, proposal: &Proposal<Self::Api>) -> ProposalStatus {
+        if proposal.was_executed {
+            return ProposalStatus::Executed;
+        }
+
+        let current_block = self.blockchain().get_block_nonce();
+        let proposal_block = proposal.creation_block;
+        let voting_period = self.voting_period().get();
+
+        let voting_start = proposal_block;
+        let voting_end = voting_start + voting_period;
+
+        if current_block < voting_start {
+            return ProposalStatus::Pending;
+        }
+        if current_block >= voting_start && current_block < voting_end {
+            return ProposalStatus::Active;
+        }
+
+        let total_upvotes = &proposal.num_upvotes;
+        let total_downvotes = &proposal.num_downvotes;
+        let quorum = self.quorum().get();
+
+        if total_upvotes > total_downvotes && total_upvotes - total_downvotes >= quorum {
+            ProposalStatus::Succeeded
+        } else {
+            ProposalStatus::Defeated
+        }
+    }
 }
