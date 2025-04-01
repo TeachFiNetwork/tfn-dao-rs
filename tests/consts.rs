32addd16814c2dab8c5b1759644ba3b063473ff0
Staking Agency <contact@staking.agency>
2025-04-01 18:36:56 +0300

    removed tests

--- tests/consts.rs
+++
@@ -1,7 +0,0 @@
-pub const TFN_DAO_ADDRESS_EXPR: &str = "sc:tfn_dao";
-pub const TFN_DAO_PATH_EXPR: &str = "mxsc:output/tfn-dao.mxsc.json";
-pub const GOVERNANCE_TOKEN_ID_EXPR: &str = "str:LEARN-123456";
-pub const GOVERNANCE_TOKEN_ID: &str = "LEARN-123456";
-
-pub const OWNER_ADDRESS_EXPR: &str = "address:owner";
-pub const CALLER_ADDRESS_EXPR: &str = "address:caller";
